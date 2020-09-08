fb686a180c6c834942281af40c7ae8b58375716d
Manuel Pacheco <manuelalejandropm@gmail.com>
2020-09-08 00:51:36 -0300

    initial commit. sample project

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(Time.deltaTime * 25, Time.deltaTime * 25, 0));
+    }
+}
